42f8f6409f3fb9b21d6193db5bf13dde75e5d048
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-05-22 21:09:33 -0300

    .

--- vila-monari/routes/api.php
+++ vila-monari/routes/api.php
@@ -7,12 +7,12 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::post('/login', LoginController::class);
-Route::post('/logout', LogoutController::class)->middleware('auth.sanctum');
+Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
 
 Route::post('/register', [UserController::class, 'store']);
 
-Route::get('user', [UserController::class, 'show'])->middleware('auth.sanctum');
-Route::put('user', [UserController::class, 'update'])->middleware('auth.sanctum');
-Route::delete('user', [UserController::class, 'destroy'])->middleware('auth.sanctum');
+Route::get('user', [UserController::class, 'show'])->middleware('auth:sanctum');
+Route::put('user', [UserController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('user', [UserController::class, 'destroy'])->middleware('auth:sanctum');
 
 Route::apiResource('/posts', PostController::class);
